1caa11a6e94e4ad9df4debad8d2d50565e8913b6
milomnz <jushajuan7@gmail.com>
2025-02-05 07:16:51 -0500

    Primmer commit

---
+++ arreglosEstaticos.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int arreglo[5] = {2,3,4,5,6};
+	for (int i =0; i<5; i++){
+		cout << arreglo[i] << '\t';
+	}
+	return 0;
+}
---
+++ matrices.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int matriz[2][3]= {{2,1,4},{5,6,7}};
+cout << matriz[1][1];
+
+}
